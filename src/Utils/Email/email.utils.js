1127f6a178715244a9b21fc5323bdd3e774e6ccb
mohamedFouadgebil <mohamed.foouad987@gmail.com>
2026-05-06 19:26:45 +0300

    Update email.utils.js

--- src/Utils/Email/email.utils.js
+++ src/Utils/Email/email.utils.js
@@ -2,15 +2,13 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
     tls: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, 
     },
   });
 
